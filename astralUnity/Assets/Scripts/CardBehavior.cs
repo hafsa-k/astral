e00bdd74478670aefcd766e858158742b13a1589
Hafsa Khallouk <hafsa-k@hotmail.com>
2023-10-25 16:08:39 +0200

    position card, coding over and clic

--- astralUnity/Assets/Scripts/CardBehavior.cs
+++ astralUnity/Assets/Scripts/CardBehavior.cs
@@ -1,85 +1,85 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-// [RequireComponent(typeof(SpriteRenderer))]
-// [RequireComponent(typeof(Animator))]
-// public class CardBehavior : MonoBehaviour
-// {
-//    public Sprite faceUpSprite;
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class CardBehavior : MonoBehaviour
+{
+   public Sprite faceUpSprite;
 
-//    private Sprite faceDownSprite;
+   private Sprite faceDownSprite;
 
-//    private Animator animator;
+   private Animator animator;
 
-//    private bool _mouseIsOver;
-//    private bool _isClicked;
+   private bool _mouseIsOver;
+   private bool _isClicked;
 
-//    public int id;
+   public int id;
 
-//    public InputActionAsset actions;
+   public InputActionAsset actions;
 
-//    [HideInInspector]
-//    public CardManager manager;
+   [HideInInspector]
+   public CardManager manager;
 
-//    public bool mouseIsOver{
-//       get {return _mouseIsOver;}
-//       set {
-//          _mouseIsOver = value;
-//          animator.SetBool("mouse is over", value);
-//       }
-//    }
+   public bool mouseIsOver{
+      get {return _mouseIsOver;}
+      set {
+         _mouseIsOver = value;
+         animator.SetBool("mouse is over", value);
+      }
+   }
 
-//    public bool isClicked{
-//       get {return _isClicked;}
-//       set {
-//          _isClicked = value;
-//          animator.SetBool("is clicked", value);
-//       }
-//    }
+   public bool isClicked{
+      get {return _isClicked;}
+      set {
+         _isClicked = value;
+         animator.SetBool("is clicked", value);
+      }
+   }
 
-//    private void Start(){
-//       faceDownSprite = GetComponent<SpriteRenderer>().sprite;
-//       animator = GetComponent<Animator>();
+   private void Start(){
+      faceDownSprite = GetComponent<SpriteRenderer>().sprite;
+      animator = GetComponent<Animator>();
 
-//    }
+   }
 
    
-//     void OnEnable(){
-//         actions.FindActionMap("card actions map").Enable();
-//         actions.FindActionMap("card actions map").FindAction("returns card").performed += OnMouseClicked;
-//     }
+    void OnEnable(){
+        actions.FindActionMap("card actions map").Enable();
+        actions.FindActionMap("card actions map").FindAction("returns card").performed += OnMouseClicked;
+    }
 
-//     void OnDisable(){
-//         actions.FindActionMap("card actions map").Disable();
-//         actions.FindActionMap("card actions map").FindAction("returns card").performed -= OnMouseClicked;
+    void OnDisable(){
+        actions.FindActionMap("card actions map").Disable();
+        actions.FindActionMap("card actions map").FindAction("returns card").performed -= OnMouseClicked;
         
-//     }
+    }
 
-//    public void Switch(){
-//       GetComponent<SpriteRenderer>().sprite = faceUpSprite;
-//    }
+   public void Switch(){
+      GetComponent<SpriteRenderer>().sprite = faceUpSprite;
+   }
 
-//    public void UnSwitch(){
-//       GetComponent<SpriteRenderer>().sprite = faceDownSprite;
-//    }
+   public void UnSwitch(){
+      GetComponent<SpriteRenderer>().sprite = faceDownSprite;
+   }
 
-//    private void OnMouseEnter(){
-//       mouseIsOver = true;
-//    }
+   private void OnMouseEnter(){
+      mouseIsOver = true;
+   }
 
-//    private void OnMouseExit(){
-//       mouseIsOver = false;
-//    }
+   private void OnMouseExit(){
+      mouseIsOver = false;
+   }
 
-//    private void OnMouseClicked(InputAction.CallbackContext context){
-//       if(mouseIsOver) {
-//          isClicked = true;
-//          manager.FaceUp(this);  
+   private void OnMouseClicked(InputAction.CallbackContext context){
+      if(mouseIsOver) {
+         isClicked = true;
+         manager.FaceUp(this);  
          
 
-//       } 
-//    }
+      } 
+   }
    
-// }
+}
--- astralUnity/Assets/Scripts/CardManager.cs
+++ astralUnity/Assets/Scripts/CardManager.cs
@@ -1,75 +1,67 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class CardManager : MonoBehaviour
-// {
+public class CardManager : MonoBehaviour
+{
 
-//     public List<GameObject> deck = new();
+    public List<GameObject> deck = new();
 
-//     //not necessary yet
-//     private List<int> indexesAlreadyInGame = new();
-//     private List<int> indexesNotAllowedAnymore = new();
+    //not necessary yet
+    private List<int> indexesAlreadyInGame = new();
+    private List<int> indexesNotAllowedAnymore = new();
+    public int nCards = 22;
+
+    void Start()
+    {
+        InstanciateCards();
+    }
+
+    //revoir cette partie la pour l'affichage
+    void InstanciateCards()
+    {
+        float cardSpacing = 2f; // Espacement entre les cartes (ajustez selon vos besoins)
+        Vector3 offset = new Vector3(-20, -2, 0);
+
+        for (int line = 0; line < nCards; line++)
+        {
+            Vector3 position = offset + Vector3.right * (line * cardSpacing);
+            InstanciateCard(position);
+        }
+    }
+
+    void InstanciateCard(Vector3 position)
+    {
+
+        int index;
+        do
+        {
+            index = Random.Range(0, deck.Count);
+        }
+        while (indexesNotAllowedAnymore.Contains(index));
+
+        
+        indexesNotAllowedAnymore.Add(index);
 
 
-//     void Start()
-//     {
-//         InstanciateCards();
-//     }
+        GameObject card = Instantiate(deck[index], position, Quaternion.identity) as GameObject;
+        card.GetComponent<CardBehavior>().manager = this;
 
-//     //revoir cette partie la pour l'affichage
-//     void InstanciateCards()
-//     {
-//         Vector3 offset = new(-3, -3, 0);
+    }
 
-//         for (int line = 0; line < 3; line++)
-//         {
-//             for (int column = 0; column < 4; column++)
-//             {
-//                 Vector3 position = offset + Vector3.right * column * 2f + Vector3.up * line * 3;
-//                 InstanciateCard(position);
-//             }
-//         }
-//     }
+    public void FaceUp(CardBehavior card)
+    {
+        StartCoroutine(_FaceUp(card));
+    }
 
-//     void InstanciateCard(Vector3 position)
-//     {
-
-//         int index;
-//         do
-//         {
-//             index = Random.Range(0, deck.Count);
-//         }
-//         while (indexesNotAllowedAnymore.Contains(index) ||
-//         indexesAlreadyInGame.Count >= 6 && !indexesAlreadyInGame.Contains(index));
-
-//         if (indexesAlreadyInGame.Contains(index))
-//         {
-//             indexesNotAllowedAnymore.Add(index);
-//         }
-//         else
-//         {
-//             indexesAlreadyInGame.Add(index);
-//         }
-
-//         GameObject card = Instantiate(deck[index], position, Quaternion.identity) as GameObject;
-//         card.GetComponent<CardBehavior>().manager = this;
-
-//     }
-
-//     public void FaceUp(CardBehavior card)
-//     {
-//         StartCoroutine(_FaceUp(card));
-//     }
-
-//     private IEnumerator _FaceUp(CardBehavior card)
-//     {
-//         Debug.Log($"card face up: {card.name}");
-//         yield return new WaitForSeconds(0.5f);
+    private IEnumerator _FaceUp(CardBehavior card)
+    {
+        Debug.Log($"card face up: {card.name}");
+        yield return new WaitForSeconds(0.5f);
 
     
-//     }
-// }
+    }
+}
 
 
 

3e23359541b77b569321c4c6d22197441e7c9e06
ShantyLony <129935832+ShantyLony@users.noreply.github.com>
2023-10-26 16:27:52 +0200

    Delete wwwhafsa directory

--- wwwhafsa/connexion/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DBDRIVER','mysql');
-define('DBHOST','localhost');
-define('DBPORT',3306);
-define('DBNAME','loginAstral');
-define('DBCHARSET','utf8');
-define('DBUSER','root');
-define('DBPASS','');

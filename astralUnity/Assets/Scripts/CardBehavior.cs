c2f28f6e40098f0fb0402ae24e4e38fe2a7cb966
Hafsa Khallouk <hafsa-k@hotmail.com>
2023-10-26 16:37:04 +0200

    ajouter du script ID web mais il manque l'id

--- astralUnity/Assets/Scripts/CardBehavior.cs
+++ astralUnity/Assets/Scripts/CardBehavior.cs
@@ -111,6 +111,7 @@ public class CardBehavior : MonoBehaviour
          isClicked = true;
          manager.FaceUp(this);  
          
+         
          CardBehavior.hasBeenChosen = true;
 
       

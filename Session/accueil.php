e3f180a9e4af7f90b91ac7c0fa92424de755b0e0
Emanuela Cardu <emanuelacardu@gmail.com>
2023-10-26 15:44:24 +0200

    historique final propre!

--- Session/accueil.php
+++ Session/accueil.php
@@ -22,65 +22,43 @@
             // Construire l'URL avec l'ID incrusté
             var url = "https://localhost/astral/Session/accueil.php?id_carte=" + id_carte;
 
-            // Ouvrir l'URL dans une nouvelle fenêtre ou un nouvel onglet
+            // Ouvrir l'URL dans la même fenêtre 
             window.location.href = url;
         });
     </script>
 
-        <!-- /* <script>
-    function ouvrirURL() {
-        var id_carte = 1; // Remplacez cette valeur par l'ID souhaité
-
-        // Construire l'URL avec l'ID incrusté
-        var url = "https://localhost/astral/Session/accueil.php?id_carte=" + id_carte;
-
-        // Ouvrir l'URL dans une nouvelle fenêtre ou un nouvel onglet
-        // window.open(url);
-        window.location.href = url;
-    }</script>*/ -->
 
     <?php
 
 
-// Étape 2 : Récupérer l'utilisateur à partir de la session
+// Récupérer l'utilisateur à partir de la session
 if (isset($_SESSION['id'])) {
     $user_id = $_SESSION['id'];
 } else {
     die("Utilisateur non connecté.");
 }
 
-// 2. Connecter à la BD
-// Connecter à la BD (loginpassbd, tableau Utilisateur)
+// Connecter à la BD
+// 
 include "./connexion/db.php";
 
 try {
     $cnx = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset=' . DBCHARSET, DBUSER, DBPASS);
 } catch (Exception $e) {
-    // jamais en production car ça montre des infos
-    // sensibles
 
-    // echo $e->getMessage();
     die();
 }
 
-// Étape 4 : Insérer l'ID dans la base de données
-    // Vous devrez adapter la requête SQL en fonction de votre schéma de base de données
-    // Étape 1 : Récupérer l'ID depuis l'URL
+// Insérer l'ID dans la base de données
+
+    //  Récupérer l'ID depuis l'URL
 if (isset($_GET['id_carte'])) {
-    // $id_url = intval($_GET['id']);
     $id_url = (int)$_GET['id_carte'];
     $sql = "INSERT INTO historique (id_utilisateur, id_carte, dateTir) VALUES (?, ?,  NOW())";
     $stmt = $cnx->prepare($sql);
     $stmt->execute([$user_id, $id_url]);
 }
     ?>
-
-
-
-
-
-
-
 
 
 </body>
